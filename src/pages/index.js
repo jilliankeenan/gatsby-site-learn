4e8562f710d2742bab7c5e9d5842146aea1a5645
Jillian Keenan <Jillian.Keenan@liberty-it.co.uk>
2018-08-13 12:15:25 +0100

    inital commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Link from "gatsby-link"
+
+
+export default () => <div style={{ color: 'pink' }}>Hello Jill!
+<h1> This is fun </h1>
+<p> Testing testing 123 </p>
+
+<img src="https://source.unsplash.com/random/400x200" alt="" />
+<br />
+<div>
+<Link to="/page-2/"> Click Me </Link>
+    </div>
+
+    <div>
+<Link to="/counter/"> Counter </Link>
+    </div>
+</div>
